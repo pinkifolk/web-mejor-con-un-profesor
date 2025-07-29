c2cd13499ad1f315a1ca521dafb561ef93fd5dbd
Sebastian Solis Cerda <89028190+pinkifolk@users.noreply.github.com>
2025-07-29 16:35:38 -0400

    refactor: organize configuration and ensure proper formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,27 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node'
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'server',
-    adapter : node({
-        mode: 'standalone'
-    }),
-    server: {
-        host: true,
-        port: process.env.PORT ? Number(process.env.PORT) : 4321
-    } 
+  output: 'server',
 
+  adapter : node({
+      mode: 'standalone'
+  }),
+
+  server: {
+      host: true,
+      port: process.env.PORT ? Number(process.env.PORT) : 4321
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  experimental :{
+    svg:{
+      mode : 'sprite'
+    }
+  }
 });
