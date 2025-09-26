ba15c32b537fbeacf036aaa7acd199d329db60dc
Sebastian Solis Cerda <89028190+pinkifolk@users.noreply.github.com>
2025-09-26 01:34:21 -0300

    feat: agregar nuevos tipos para detalles de tours, administración y disponibilidad

--- src/types/tours.ts
+++ src/types/tours.ts
@@ -1,6 +1,56 @@
+import type { a } from "node_modules/tailwindcss/dist/types-B254mqw1.d.mts";
+
 export type Tour = {
     id: number;
     name: string;
     description: string;
     img: string;
+    slug: string;
+    status: boolean;
+}
+export type TourDetail = {
+    id: number;
+    name: string;
+    description: string;
+    img: string;
+    slug: string;
+    status: boolean;
+    duration : string;
+    max_people: number;
+    itinerary: [string];
+    rating: string;
+    total_reviews: number;
+    rating_counts: {
+        rating: number;
+        percentage : number;
+    }[];  
+    comments: {
+        name: string;
+        comment: string;
+        rating: number;
+        created_at: string;
+    }[];
+}
+
+export type InsertNewTour = {
+    name: string;
+    description: string;
+    img: string;    
+    status: boolean;
+};
+export type TourAdmin = {
+    id: number;
+    name: string;
+    description: string;
+    img: string;
+    status: boolean;
+    total_bookings: number;
+}
+export type Hours = {
+    id: number;
+    hour: string;
+}
+export type Availability = {
+    total: string, 
+    disponible: string
 }
