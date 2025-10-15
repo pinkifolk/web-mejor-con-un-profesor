1906c968ad503314547edc06233089647ae4f9fe
Sebastian Solis Cerda <89028190+pinkifolk@users.noreply.github.com>
2025-10-14 23:00:44 -0300

    feat: se cambia el metodo para obtener los tours de el menu explorar y el de tours

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,11 +1,19 @@
 import { defineAction } from "astro:actions";
-import {GetDestinosPopulate} from '@/lib/db'
+import {GetDestinosPopulate, GetDestinosAll} from '@/lib/db'
 import { z } from "astro:schema";
 
 export const server = {
   GetDestinosPopulate: defineAction({
     async handler(){
       const data = await GetDestinosPopulate()
+      return JSON.parse(JSON.stringify(data));
+
+    }
+  }
+  ),
+  GetDestinosAll: defineAction({
+    async handler(){
+      const data = await GetDestinosAll()
       return JSON.parse(JSON.stringify(data));
 
     }
